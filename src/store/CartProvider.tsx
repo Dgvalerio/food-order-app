6b484c512d6569887a70edb2fec213ca762afe6c
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-16 15:48:34 -0300

    Class 143 - Adding a Cart Reducer

--- src/store/CartProvider.tsx
+++ src/store/CartProvider.tsx
@@ -1,16 +1,49 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useReducer } from 'react';
 
-import { ICartContext, IMeal } from '../interfaces';
+import { ICartContext, ICartItem } from '../interfaces';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+interface IState {
+  items: ICartItem[] | [];
+  totalAmount: number;
+}
+
+type Action = { type: 'ADD'; item: ICartItem } | { type: 'REMOVE'; id: string };
+
+const cartReducer = ({ items, totalAmount }: IState, action: Action) => {
+  switch (action.type) {
+    case 'ADD':
+      return {
+        items: [...items, action.item],
+        totalAmount: totalAmount + action.item.amount * action.item.price,
+      };
+    case 'REMOVE':
+      return defaultCartState;
+    default:
+      return defaultCartState;
+  }
+};
+
 const CartProvider = ({ children }: { children: ReactNode }): JSX.Element => {
-  const addItemToCartHandler = (item: IMeal) => {};
-  const removeItemFromCartHandler = (id: string) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item: ICartItem) =>
+    dispatchCartAction({ type: 'ADD', item });
+
+  const removeItemFromCartHandler = (id: string) =>
+    dispatchCartAction({ type: 'REMOVE', id });
 
   const cartContext: ICartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
